687416b47546140e59a36aba6229bbc92b2cb0d7
Tracy Xing <tracyxing2002@gmail.com>
2025-08-25 17:42:08 +0800

    SQL practice set up

---
+++ sql_practice/task1.sql
@@ -0,0 +1,29 @@
+-- Problem 1: Retrieve all products in the Sports category
+-- Write an SQL query to retrieve all products in a specific category.
+
+SELECT 
+    p.product_id,
+    p.category_id,
+    c.category_name
+FROM Products p
+INNER JOIN Categories c ON p.category_id = c.category_id
+Where category_name='Sports';
+
+-- Problem 2: Retrieve the total number of orders for each user
+-- Write an SQL query to retrieve the total number of orders for each user.
+-- The result should include the user ID, username, and the total number of orders.
+
+
+
+-- Problem 3: Retrieve the average rating for each product
+-- Write an SQL query to retrieve the average rating for each product.
+-- The result should include the product ID, product name, and the average rating.
+
+
+
+-- Problem 4: Retrieve the top 5 users with the highest total amount spent on orders
+-- Write an SQL query to retrieve the top 5 users with the highest total amount spent on orders.
+-- The result should include the user ID, username, and the total amount spent.
+
+
+
